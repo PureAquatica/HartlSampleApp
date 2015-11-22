f2c96f62b173a58968edd583d4a1a60b88b4a9a3
Pureaquatica <Mat.Duck@gmail.com>
2015-11-22 17:20:01 +0000

    Chapter 4 excercises complete

---
+++ chapter 4 excercises.rb
@@ -0,0 +1,32 @@
+#1 Taking and shuffling a string.
+def string_shuffle(s)
+ s.split("").shuffle.join
+end
+string_shuffle("foobar")
+
+
+#2 I ran this on the console and added this to the string class (in that instance of the console, I could not find the main string class in my lib/ext folder.)
+ class String
+   def shuffle
+    self.split("").shuffle.join
+   end
+ end
+ "foobar".shuffle
+
+
+#3 Creating person hashes
+person1 = { :first => "John", :last => "Alden" }
+person2 = { :first => "Geralt", :last => "OfRivia" }
+person3 = { :first => "Victor", :last => "Newman" }
+
+# Creating a param hash filled with person hasesh. 
+params = {
+ :father => person1,
+ :mother => person2,
+ :child  => person3
+}
+
+#4  Calling that hash will replace the value of the old be with the value of the new b.
+# http://docs.ruby-lang.org/en/2.0.0/Hash.html
+>>  { "a" => 100, "b" => 200 }.merge({ "b" => 300 })
+=> {"a"=>100, "b"=>300}
